f42eae13f9191d8dd4cf89d8aa9d7282e0c129bb
danny <morganz55057@gmail.com>
2020-01-20 22:41:26 +0100

    added pom file

--- 0.first-non-maven-project/src/App.java
+++ 0.first-non-maven-project/src/App.java
@@ -1,12 +1,13 @@
 
 public class App {
+	
 	/**
 	 * Hello world!
 	 *
 	 */
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!!");
+		System.out.println("Hello World!");
 	}
 
 	public int calculateSomething() {
